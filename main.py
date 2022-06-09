82e4d80b1cc9fdb251849a79acf5c978e0cbbb6b
Juli2709 <107204635+Juli2709@users.noreply.github.com>
2022-06-09 20:50:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+a = int(input("введите первое число:"))
+b = int(input("введите второе число:"))
+с = input("введите операцию:")
+
+if c == "+":
+    print(a+b)
+elif c =="-":
+    print(a-b)
+elif c == "*":
+    print(a*b)
+elif c == "/":
+    print(a/b)
+elif c == "%":
+    print(a%b)
+elif c == "^":
+    print(a**b)
